035418b1cc2914e227c430642950fdea8d24f729
Berlin Sky <pedroyoung8989@gmail.com>
2019-11-07 18:54:46 -0800

    add bootstrap and rounter

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,15 +1,13 @@
 import React from "react";
                          
-function AboutPage () {
-  return (             
-    <div>                    
-      <h2>About</h2>        
-      <p>                  
-        This is about doing things right, not fast.  
-      </p>                                         
-    </div>                  
-  )                   
-}                        
+const AboutPage = () => (
+  <div>                    
+    <h2>About</h2>        
+    <p>                  
+      This is about doing things right, not fast.  
+    </p>                                         
+  </div>                  
+)                        
                          
 export default AboutPage;
                          
--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,12 +1,10 @@
 import React from "react";
                           
-function CoursesPage() {
-  return (          
-    <div>                       
-      <h2>Courses</h2>         
-    </div>                    
-  )                     
-  };                        
-                          
+const CoursesPage = () => (
+  <div>                       
+    <h2>Courses</h2>         
+  </div>                    
+)  
+                           
 export default CoursesPage;
-                          
+                           
