b6f0d565b7c815986074237fcf0bb1bf26a12458
Berlin Sky <pedroyoung8989@gmail.com>
2019-11-07 18:41:45 -0800

    add bootstrap and rounter

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,11 +1,7 @@
 import React from "react";
                                                           
-// const PageNotFound = () => <h1>Sorry, Page not found.</h1>
-                            
-function PageNotFound() {
-  return (            
-    <h1>Sorry, Page not found.</h1> 
-  )                                 
-}         
+const PageNotFound = () => <h1>Sorry, Page not found!</h1>
+                                                      
+                                                     
                                                            
 export default PageNotFound;                              
