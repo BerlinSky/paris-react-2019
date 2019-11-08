981297c31ebbe30863df8d6efe80fa0312fabd80
Berlin Sky <pedroyoung8989@gmail.com>
2019-11-07 18:38:17 -0800

    add bootstrap and rounter

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,21 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";                        
 import AboutPage from "./About/AboutPage";    
 import CoursesPage from "./courses/CoursesPage";  
-import Header from "./common/Header";           
-                                               
-function App() {                              
-  return (                                   
-    <div className="container-fluid">        
-      <Header></Header>                      
-      <Switch>                           
+import Header from "./common/Header";     
+import PageNotFound from "./PageNotFound"         
+                                                  
+function App() {                                  
+  return (                                      
+    <div className="container-fluid">          
+      <Header></Header>                       
+      <Switch>                               
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/about" component={AboutPage}></Route>
         <Route path="/courses" component={CoursesPage}></Route>
-        {/* <Route path="/"></Route> */}
-      </Switch>                       
-    </div>                            
-  )                                  
-}                                 
-                                  
-export default App;           
+        <Route component={ PageNotFound }></Route>
+      </Switch>                             
+    </div>                                  
+  )                                         
+}                                           
+                                            
+export default App;               
