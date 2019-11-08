a3b4392630b6e676670284ae75b373a70263d43d
Berlin Sky <pedroyoung8989@gmail.com>
2019-11-07 16:39:52 -0800

    add bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
+// import { BrowsetRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";         
 
-function Hello() {
-  return <h2>Hello, the world</h2>
-}         
-
+function Hello() {                                         
+  return <h2 className="container">Hello, the world</h2>            
+}                                      
+                            
 render(<Hello />, document.getElementById("app"));
